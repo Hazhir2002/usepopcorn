58c1e55c689552c156f81184cc25066b0d42fd45
Hazhir2002 <Hazhiryousefi2002@gmail.com>
2023-07-17 16:06:44 +0330

    added a small feature with the Enter key event

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,21 @@ import { useRef, useEffect } from "react";
 export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <input
